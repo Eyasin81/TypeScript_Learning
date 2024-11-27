ce9918e97cf39f0a3313b0f1d7068267f8642e57
Eyasin81 <eyasina81@gmail.com>
2024-11-27 10:13:10 +0700

    data type

---
+++ 4_data_Type/index.js
@@ -0,0 +1,9 @@
+//Number
+var age = 12;
+console.log(age);
+// String
+var Name = "hello world";
+console.log(Name);
+//Boolean
+var isActive = true;
+console.log(isActive);
---
+++ 4_data_Type/index.ts
@@ -0,0 +1,10 @@
+//Number
+let age:number=12
+console.log(age)
+// String
+let Name:string="hello world"
+console.log(Name)
+//Boolean
+let isActive:boolean=true
+console.log(isActive)
+
